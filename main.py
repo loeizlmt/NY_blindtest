20ca345e68fbb01711e6bd65351f9f4c1dd9fd4c
loeizlmt <lolojardinets@outlook.fr>
2024-12-10 20:31:53 +0100

    ajout du buzzer

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import streamlit as st
 
+with open('style.css') as f:
+    css = f.read()
+
+st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+
+with open('main.html') as f:
+    html = f.read()
+
+st.write("ceci est un buzzer : ")
+
+st.markdown(f'<html>{html}</html>', unsafe_allow_html=True)
+
